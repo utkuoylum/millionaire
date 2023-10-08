277e8950748570beceac99486b7d822a1f2b045f
Etem Utku Oylum <utkuoylum@gmail.com>
2023-10-08 12:07:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <div className="app">
+    <div className="main">main</div>
+    <div className="pyramid">
+      <ul className="moneyList">
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+        <li className="moneyListItem">$ 200</li>
+      </ul>
+    </div>
+  </div>
+  )
+}
+
+export default App
